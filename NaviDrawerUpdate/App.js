9d4294f00e247feb095e275d464648a683deaaeb
abmrifat1 <rifatabm1@gmail.com>
2019-10-22 17:43:01 +0600

    navidrawerUpdate

---
+++ NaviDrawerUpdate/App.js
@@ -0,0 +1,136 @@
+//This is an example code for NavigationDrawer//
+import React, {Component} from 'react';
+//import react in our code.
+import {View, Image, TouchableOpacity, Text} from 'react-native';
+// import all basic components
+
+//For React Navigation 3+
+//import {
+//  createStackNavigator,
+//  createDrawerNavigator,
+//  createAppContainer,
+//} from 'react-navigation';
+
+//For React Navigation 4+
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createStackNavigator} from 'react-navigation-stack';
+import Screen1 from './src/container/page1';
+import Screen2 from './src/container/page2';
+import Screen3 from './src/container/page3';
+import NextPage from './src/container/Next';
+
+import NavigationDrawerStructure from './src/component/NavigationDrawerStructure';
+
+const FirstActivity_StackNavigator = createStackNavigator({
+  //All the screen from the Screen1 will be indexed here
+  First: {
+    screen: Screen1,
+    navigationOptions: ({navigation}) => ({
+      title: 'Demo Screen 1',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+const Screen2_StackNavigator = createStackNavigator({
+  //All the screen from the Screen2 will be indexed here
+  Second: {
+    screen: Screen2,
+    navigationOptions: ({navigation}) => ({
+      title: 'Demo Screen 2',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+const Screen3_StackNavigator = createStackNavigator({
+  //All the screen from the Screen3 will be indexed here
+
+  Third: {
+    screen: Screen3,
+    navigationOptions: ({navigation}) => ({
+      title: 'Demo Screen 3',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerRight: (
+        <TouchableOpacity onPress={() => navigation.navigate('Next')}>
+          <Text style={{padding: 10, color: 'white', fontSize: 25}}>Demo4</Text>
+        </TouchableOpacity>
+      ),
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+  Next: {
+    screen: NextPage,
+    navigationOptions: ({navigation}) => ({
+      title: 'Demo 4',
+      headerRight: (
+        <TouchableOpacity onPress={() => navigation.navigate('NextNaviPage')}>
+          <Text style={{padding: 10, color: 'white', fontSize: 25}}>
+            About Me
+          </Text>
+        </TouchableOpacity>
+      ),
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+
+  NextNaviPage: {
+    screen: NextPage,
+    navigationOptions: ({navigation}) => ({
+      title: 'About Me',
+      headerRight: (
+        <TouchableOpacity onPress={() => navigation.navigate('Third')}>
+          <Text style={{padding: 10, color: 'white', fontSize: 25}}>
+            1st page
+          </Text>
+        </TouchableOpacity>
+      ),
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+const DrawerNavigatorExample = createDrawerNavigator({
+  //Drawer Optons and indexing
+  Screen1: {
+    //Title
+    screen: FirstActivity_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 1',
+    },
+  },
+  Screen2: {
+    //Title
+    screen: Screen2_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 2',
+    },
+  },
+  Screen3: {
+    //Title
+    screen: Screen3_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 3',
+    },
+  },
+});
+
+export default createAppContainer(DrawerNavigatorExample);
---
+++ NaviDrawerUpdate/src/component/NavigationDrawerStructure.js
@@ -0,0 +1,27 @@
+import React from 'react';
+//import react in our code.
+import {View, Image, TouchableOpacity} from 'react-native';
+
+import DrawerImg from '../image/drawer.png';
+class NavigationDrawerStructure extends React.Component {
+  //Structure for the navigatin Drawer
+  toggleDrawer = () => {
+    //Props to open/close the drawer
+    this.props.navigationProps.toggleDrawer();
+  };
+  render() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+          {/*Donute Button Image */}
+          <Image
+            source={DrawerImg}
+            style={{width: 25, height: 25, marginLeft: 5}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default NavigationDrawerStructure;
