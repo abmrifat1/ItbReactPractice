0bab3c398e6772637c9bd744d68aed2cf1af4b38
abmrifat1 <rifatabm1@gmail.com>
2019-11-16 14:43:23 +0600

    model done

--- ModalInNative/App.js
+++ ModalInNative/App.js
@@ -1,114 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
+//import React
+
 import {
-  SafeAreaView,
+  Text,
   StyleSheet,
   ScrollView,
   View,
-  Text,
-  StatusBar,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  // Modal,
 } from 'react-native';
+//import React Native Component
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Modal from 'react-native-modalbox';
+//import Modal for different modal box
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+var screen = Dimensions.get('window');
+//Screen Dimention for list width used below
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+      isDisabled: false,
+      swipeToClose: true,
+      sliderValue: 0.3,
+    };
+  }
+
+  onClose() {
+    //called on modal closed
+    console.log('Modal just closed');
+  }
+
+  onOpen() {
+    //called on modal opened
+    console.log('Modal just opened');
+  }
+
+  onClosingState(state) {
+    //called on modal close/open of the swipe to close change
+    console.log('Open/Close of the SwipeToClose just changed');
+  }
+
+  renderList() {
+    //function to return List (To show on Modal)
+    var list = [];
+    for (var i = 0; i < 50; i++) {
+      list.push(
+        <Text style={styles.text} key={i}>
+          Item {i}
+        </Text>,
+      );
+    }
+    return list;
+  }
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.setState({isOpen: true})}>
+          <Text style={styles.buttonText}>Open Modal</Text>
+        </TouchableOpacity>
+
+        <Modal
+          isOpen={this.state.isOpen}
+          onClosed={() => this.setState({isOpen: false})}
+          style={[styles.modal, styles.modal4]}
+          position={'center'}>
+          <Text style={styles.text}>Modal </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.setState({isOpen: false})}>
+            <Text style={styles.buttonText1}>close Modal</Text>
+          </TouchableOpacity>
+        </Modal>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  wrapper: {
+    paddingTop: 50,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
   },
-  body: {
-    backgroundColor: Colors.white,
+  modal2: {
+    height: 230,
+    width: 300,
+    backgroundColor: 'orange',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  modal3: {
+    height: 300,
+    width: 300,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  modal4: {
+    height: 300,
+    width: 300,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  text: {
+    color: 'black',
+    fontSize: 22,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: '#DDDD',
+    width: 100,
+    marginTop: 16,
+    textAlign: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'center',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    color: 'green',
+  },
+  buttonText1: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    color: 'red',
   },
 });
-
-export default App;
