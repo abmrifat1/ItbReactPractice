6eac4ab53ac02d99f29def38e9942031d79e916a
abmrifat1 <rifatabm1@gmail.com>
2019-11-18 15:05:37 +0600

    map sucessful

---
+++ MapYoutube/App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+export default App;
